085860e9f4f0b5dcab8bcf924940e8b35dad1171
Yannickvdv <y.vandervleuten@student.fontys.nl>
2019-03-22 00:04:49 +0100

    Create UniqueConstraintViolationException.java

---
+++ src/main/java/common/exceptions/UniqueConstraintViolationException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Yannick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package common.exceptions;
+
+/** 
+ * User.Name is @unique, meaning it will throw an error if a {@link user} 
+ * is persisted while the database contains another user with that name.
+ *
+ * @author Yannick
+ */
+public class UniqueConstraintViolationException extends Exception {
+    
+    public UniqueConstraintViolationException (String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public UniqueConstraintViolationException (String message) {
+        super(message);
+    }
+}
