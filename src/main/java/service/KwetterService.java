b9db1d8a0ef9fb22ce39f29184eaac342da86d2f
Yannickvdv <y.vandervleuten@student.fontys.nl>
2019-05-10 11:23:27 +0200

    Fixed deploy issues, fixed bug

--- src/main/java/service/KwetterService.java
+++ src/main/java/service/KwetterService.java
@@ -22,45 +22,47 @@ import javax.inject.Inject;
  */
 @Stateless
 public class KwetterService {
-    
+
     @Inject
     private HashTagDao hashTagDAO;
     @Inject
     private UserDao userDAO;
     @Inject
     private TweetDao tweetDAO;
-    
+
     /**
-     * Add a new {@link Tweet} to an existing {@link User}
-     * Mentions and HashTags are parsed out and added 
-     * 
+     * Add a new {@link Tweet} to an existing {@link User} Mentions and HashTags
+     * are parsed out and added
+     *
      * @param tweet The {@link Tweet} to be added
      */
     public void tweet(Tweet tweet) {
         //Tweet
         this.tweetDAO.addTweet(tweet);
-        
+
         //Add Tweets to the existing HashTags
-        List<String> usedHashTags = TextHelper.searchHashTags(tweet.getText());
+        List<String> usedHashTagStrings = TextHelper.searchHashTags(tweet.getText());
         List<HashTag> availableHashTags = this.hashTagDAO.getHashTags();
-        
+
         //Loop through used hastags and add tweet to them if applicable
-        for(int i = 0; i < usedHashTags.size(); i++){
-            for(int j = 0; j < availableHashTags.size(); j++){
-                if(usedHashTags.get(i).equals(availableHashTags.get(j))){
-                    availableHashTags.get(j).addTweet(tweet);
-                    usedHashTags.remove(i);
-                    j--;
-                    break;
-                }
+        usedHashTagStrings.forEach(used -> {
+            
+            HashTag foundHashTag = availableHashTags.stream()
+                    .filter(available -> used.equals(available.getText()))
+                    .findFirst()
+                    .orElse(null);
+            
+            if (foundHashTag != null) {
+                foundHashTag.addTweet(tweet);
+                usedHashTagStrings.remove(used);
             }
-        }
-        
+        });
+
         //Add new HashTags
-        usedHashTags.forEach((String s) -> {
+        usedHashTagStrings.forEach((String s) -> {
             this.hashTagDAO.addHashTag(new HashTag(s, tweet));
         });
-        
+
         //Add the mentions to the user
         TextHelper.searchMentionedUsers(tweet.getText(), this.userDAO.getUsers()).forEach((User u) -> {
             u.addMention(tweet);
